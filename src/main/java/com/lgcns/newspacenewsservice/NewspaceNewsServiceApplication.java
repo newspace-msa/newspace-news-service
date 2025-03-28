0a2c76ae492bc6d70be2f3128670a4489a5a187c
hyun minyoung <kingmandoo95@gmail.com>
2025-03-28 16:11:01 +0900

    git init

---
+++ src/main/java/com/lgcns/newspacenewsservice/NewspaceNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.newspacenewsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaceNewsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspaceNewsServiceApplication.class, args);
+    }
+
+}
